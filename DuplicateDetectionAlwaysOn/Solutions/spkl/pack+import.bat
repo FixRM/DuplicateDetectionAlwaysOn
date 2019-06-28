a524b111ea08e3d916b0412b7872e6be5d8eefa4
Artem Grunin <FixRM@users.noreply.github.com>
2019-06-28 14:08:46 +0500

    Add support for both solution packages type

--- DuplicateDetectionAlwaysOn/Solutions/spkl/pack+import.bat
+++ DuplicateDetectionAlwaysOn/Solutions/spkl/pack+import.bat
@@ -8,7 +8,12 @@ For /R %package_root% %%G IN (spkl.exe) do (
 
 :continue
 @echo Using '%spkl_path%' 
-REM spkl workflow [path] [connection-string] [/p:release]
-"%spkl_path%" workflow "%cd%\.."
+REM spkl instrument [path] [connection-string] [/p:release]
+"%spkl_path%" import "%cd%\.."
+
+if errorlevel 1 (
+echo Error Code=%errorlevel%
+exit /b %errorlevel%
+)
 
 pause
